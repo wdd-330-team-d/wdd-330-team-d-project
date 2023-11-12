1ce26dae0c78e94cb2dfbbcb790d0ffdcb3bf711
E Brent Nelson <ebrentnelson@gmail.com>
2023-11-11 20:26:27 -0700

    Fix issues

--- dist/assets/productList-893bb92f.js
+++
@@ -1,9 +0,0 @@
-import{g as i}from"./externalServices-db2f91cb.js";import{c}from"./utils-e2829c12.js";async function d(e,a){const r=document.querySelector(e),t=await i(a);console.log(t),c(n,r,t),document.querySelector(".title").innerHTML=a}function n(e){return`<li class="product-card">
-  <a href="../product_pages/index.html?product=${e.Id}">
-    <img src="${e.Images.PrimaryMedium}" alt="${e.Name}"/>
-    <h3 class="card__brand">${e.Brand.Name}</h3>
-    <h2 class="card__name">${e.NameWithoutBrand}</h2>
-    <p class="product-card__price">Price: $${e.FinalPrice}</p></a>
-    <p class="discountIndicator">Discount: $${((e.SuggestedRetailPrice-e.FinalPrice)/e.SuggestedRetailPrice*e.FinalPrice).toFixed(2)}</p>
-  <!-- Add a container for the discount indicator -->
-  </li>`}export{d as p};
--- dist/assets/superscript-86df5b0d.js
+++
@@ -1,1 +0,0 @@
-import{g as c,q as t}from"./utils-e2829c12.js";const o=c("so-cart")||[];function s(){var r=o.length;t("#superscript").textContent=r}o.length!=0&&(t("#superscript").style.display="block",s());
