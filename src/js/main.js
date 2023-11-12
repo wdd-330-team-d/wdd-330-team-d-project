b38a36477fa458c6cebfe3ad1cc1dd4c7f22c530
E Brent Nelson <ebrentnelson@gmail.com>
2023-11-11 20:26:27 -0700

    Fix issues

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,9 @@
-import productL from "./productList.mjs";
-import { qs, loadHeaderFooter } from "./utils.mjs";
+import productL from './productList.mjs';
+import { qs, loadHeaderFooter } from './utils.mjs';
 
-const mainHeader = qs("#main-header");
-const footer = qs("footer");
-
+const mainHeader = qs('#main-header');
+const footer = qs('footer');
 
 loadHeaderFooter(mainHeader, footer);
 
-productL();
+productL('.product-list', 'tents');
